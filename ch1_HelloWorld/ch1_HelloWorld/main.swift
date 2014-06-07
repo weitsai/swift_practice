15006278c84b316ab0208e014d0b032f78d9cb51
weitsai <begining1003@gmail.com>
2014-06-07 13:42:22 +0800

    Create ch1-HelloWorld Project

---
+++ ch1_HelloWorld/ch1_HelloWorld/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  ch1_HelloWorld
+//
+//  Created by weitsai on 2014/6/7.
+//  Copyright (c) 2014年 weitsai. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
+// 宣告變數自動決定型態
+var userName = "weitsai"
+println("Hello, " + userName)
+
+
