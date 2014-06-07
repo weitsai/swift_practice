67f9e89450d15d7a383cfaed2a316865175529e6
weitsai <begining1003@gmail.com>
2014-06-07 14:16:26 +0800

    新增 Int 和 String 同時 Print 的方法與問題

--- ch1_HelloWorld/ch1_HelloWorld/main.swift
+++ ch1_HelloWorld/ch1_HelloWorld/main.swift
@@ -25,3 +25,15 @@ println(old);
 let height:Int = 169
 // len 宣告的變數不可以更改數值會發生錯誤
 // height = 200
+
+
+// 字串與數字相加
+// 下面這段程式碼會發生錯誤 (Could not find an overload for 'old' that accepts the supplied arguments)
+//println(userName + "age was" + old + "years old")
+
+// 如果要輸出, 必須把 Int 轉成 String
+println(userName + "age was " + String(old) + " years old")
+// 也可以縮寫成這樣
+println(userName + "age was \(old) years old")
+
+
